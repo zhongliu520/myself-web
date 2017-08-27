a2238c22280419b03100de9923c6209d0c481cdf
zhong.liu <zhong.liu@bioon.com>
2017-08-27 23:01:30 +0800

    ====

--- resources/views/activity/index.blade.php
+++ resources/views/activity/index.blade.php
@@ -5,6 +5,20 @@
             <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
             <title>约吗？</title>
     </head>
+    <link rel="stylesheet" href="/activity/css/index.css" />
     <body>
+    	<div class="all">
+	    	<div class="picture">
+	    		<div class="picture-li">
+	    			<img src="/activity/image/IMG_0202.JPG" />
+	    		</div>
+	    	</div>
+	    	<div class="but-yue">
+	    		<button>约</button>
+	    	</div>
+	    	<div class="but-no-yue">
+	    		<button>不约</button>
+	    	</div>
+    	</div>
     </body>
 </html>
